30e14296840862bc2992f14b8b09934dabc1ce21
rodopoulos <felipekss@gmail.com>
2023-05-02 01:08:19 -0300

    Fix DNS redirect

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,10 +2,14 @@ resource "aws_route53_zone" "feliperodopoulos_com" {
   name = "feliperodopoulos.com"
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "base" {
   zone_id = aws_route53_zone.feliperodopoulos_com.zone_id
-  name    = "www.feliperodopoulos.com"
-  type    = "CNAME"
-  ttl     = 30
-  records = [aws_s3_bucket.feliperodopoulos_com.website_endpoint]
+  name    = "feliperodopoulos.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket.feliperodopoulos_com.website_domain
+    zone_id                = aws_s3_bucket.feliperodopoulos_com.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
